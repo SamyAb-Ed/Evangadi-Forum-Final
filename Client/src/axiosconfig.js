2c2f8e673cdf3d8e280b6945e79b7a49e0bd2ff4
SamyAb-Ed <samyemahhy@gmail.com>
2025-10-26 14:49:27 -0500

    Add debugging to axiosconfig to troubleshoot API URL issue

--- Client/src/axiosconfig.js
+++ Client/src/axiosconfig.js
@@ -1,5 +1,10 @@
 import axios from "axios";
+
+// Debug: Log the API base URL
+console.log("API Base URL:", import.meta.env.VITE_API_BASE_URL);
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
 });
+
 export default axiosInstance;
